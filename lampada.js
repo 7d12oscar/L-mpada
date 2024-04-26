2ca55cedffd10e34e5cf5f11b7891d529ac769f2
7d12oscar <167602388+7d12oscar@users.noreply.github.com>
2024-04-26 04:32:16 -0300

    Add files via upload

---
+++ lampada.js
@@ -0,0 +1,10 @@
+function acendeLampada()
+{
+    document.getElementById('imagem').src='https://www.w3schools.com/js/pic_bulbon.gif'
+
+}
+
+function apagaLampada()
+{
+    document.getElementsById('imagem').src='https://www.w3schools.com/js/pic_bulboff.gif'
+}
